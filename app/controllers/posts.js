f43e4f55f198be28206afa36c7bfa4f5ebaf8f47
rotondozer <nickrotondo724@gmail.com>
2017-09-13 21:34:25 -0400

    Adds 'Posts' controller file

---
+++ app/controllers/posts.js
@@ -0,0 +1,32 @@
+'use strict'
+
+const controller = require('lib/wiring/controller')
+const models = require('app/models')
+const Post = models.post
+
+const authenticate = require('./concerns/authenticate')
+const setUser = require('./concerns/set-current-user')
+const setModel = require('./concerns/set-mongoose-model')
+
+const create = (req, res, next) => {
+  const post = Object.assign(req.body.post, {
+    _owner: req.user._id
+  })
+  Post.create(post)
+    .then(post => {
+      res.status(201)
+        .json({
+          post: post.toJSON({ user: req.user })
+        })
+    })
+    .catch(next)
+}
+
+module.exports = controller({
+  create
+}, { before: [
+  { method: setUser, only: ['index', 'show'] },
+  { method: authenticate, except: ['index', 'show'] },
+  { method: setModel(Post), only: ['show'] },
+  { method: setModel(Post, { forUser: true }), only: ['update', 'destroy'] }
+] })
